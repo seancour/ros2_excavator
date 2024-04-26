c563d3c063f3b8d62db4f65cdb298a6562117364
Sean Courtney <seancour@buffalo.edu>
2024-04-26 11:40:45 -0400

    Uploading all stl files for urdf file

--- src/excavator_model/setup.py
+++ src/excavator_model/setup.py
@@ -12,12 +12,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name), glob('urdf/*.urdf'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='sim_user',
-    maintainer_email='christo12aluckal@gmail.com',
+    maintainer_email='seancour@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
